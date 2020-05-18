f0fb2b5497f55ef4f6a4b78cb3fd21384efdfc97
Winnie Tsou <winnie22935@gmail.com>
2020-05-18 22:38:41 +0800

    Weather page is done.

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -45,7 +45,7 @@ class DataController extends Controller
             }
             $sql .= "`county_id` = " . $counties[count($counties) - 1] . " ";
 
-            if ($chart == 'total') {
+            if ($chart === 'total') {
                 $order = DB::table('case')
                             ->select("county_id AS c_id",
                                      "county_code.description AS c_name",
@@ -55,7 +55,7 @@ class DataController extends Controller
                             ->groupBy('county_id')
                             ->orderBy('county_id', 'asc')
                             ->get();
-            } else if ($chart == 'speeding') {
+            } else if ($chart === 'speeding') {
                 $sql = '(' . $sql . ')';
 
                 $order = DB::table('vehicle')
@@ -68,7 +68,7 @@ class DataController extends Controller
                             ->groupBy('county_id')
                             ->orderBy('county_id', 'asc')
                             ->get();
-             } else if ($chart == 'intersection') {
+             } else if ($chart === 'intersection') {
                 $sql = '(' . $sql . ')';
                 $order = DB::table('case')
                             ->select('county_id as c_id',
@@ -101,7 +101,7 @@ class DataController extends Controller
 
         $condition = substr($condition, 0, strrpos($condition ,'OR'));
 
-        if ($chart == 'total') {
+        if ($chart === 'total') {
             
             $order = DB::table('case')
                     ->select(DB::raw("YEAR(`accident_date`) AS 'year', count(*) AS 'count'"))
@@ -109,7 +109,7 @@ class DataController extends Controller
                     ->groupBy(DB::raw('YEAR(`accident_date`)'))
                     ->get();
 
-        } else if ($chart == 'month') {
+        } else if ($chart === 'month') {
 
             $order = DB::table('case')
                     ->select(DB::raw("YEAR(`accident_date`) AS 'year', MONTH(`accident_date`) AS 'month', count(*) AS 'count'"))
@@ -118,7 +118,7 @@ class DataController extends Controller
                     ->orderBy('year')
                     ->orderBy('month')
                     ->get();
-        } else if ($chart == 'holiday') {
+        } else if ($chart === 'holiday') {
 
             $order = DB::table('case')
                     ->select(DB::raw("YEAR(`accident_date`) AS 'year', `holiday_code`.`id` AS 'h_id', `holiday_code`.`description` AS 'holiday', count('holiday') AS 'count'"))
@@ -128,7 +128,7 @@ class DataController extends Controller
                     ->orderBy('year', 'asc')
                     ->orderBy('case.holiday_related', 'asc')
                     ->get();
-        } else if ($chart == 'death') {
+        } else if ($chart === 'death') {
 
             $condition = str_replace('accident_date', 'time', $condition);
             $order = DB::table('died')
@@ -144,7 +144,55 @@ class DataController extends Controller
     }
 
     private function weather(Request $request) {
-        
+        $chart = $request->query('chart');
+        $queryString = explode('&', $_SERVER['QUERY_STRING']);
+        $typeArray = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12];
+        $condition = "";
+
+        foreach ($queryString as $type) {
+            $type = substr($type, strpos($type, '=')+1, strlen($type));
+            if ( array_search($type, $typeArray) !== false ) {
+                $condition .= "`weather_code` = " . $type . " OR ";
+            }
+        }
+        $condition = substr($condition, 0, strrpos($condition ,'OR'));
+
+        if ($chart === 'total') {
+            $order = DB::table('weather')
+                    ->select(DB::raw("SUBSTRING(`casenum`, 1, 4) AS 'year', `weather_code` AS 'w_code', `description` AS 'weather', COUNT(`description`) AS 'count' "))
+                    ->leftJoin('weather_code', 'weather.weather_code', '=', 'weather_code.id')
+                    ->whereRaw($condition)
+                    ->groupby('year')
+                    ->groupby('w_code')
+                    ->orderBy('year', 'asc')
+                    ->orderBy('w_code', 'asc')
+                    ->get();
+        } else if ($chart === 'light') {
+            $order = DB::table('weather')
+                    ->select(DB::raw("`weather_code` AS 'w_code', `weather_code`.`description` AS 'weather', `case`.`light_condition` AS 'l_condition', `light_condition_code`.`description`, COUNT(*) AS 'count' "))
+                    ->leftJoin('weather_code', 'weather.weather_code', '=', 'weather_code.id')
+                    ->leftJoin('case', 'weather.casenum', '=', 'case.casenum')
+                    ->leftJoin('light_condition_code', 'case.light_condition', '=', 'light_condition_code.id')
+                    ->whereRaw('(`case`.`light_condition` BETWEEN 0 AND 6) AND ' . $condition)
+                    ->groupby('w_code')
+                    ->groupby('case.light_condition')
+                    ->orderBy('w_code', 'asc')
+                    ->orderBy('case.light_condition', 'asc')
+                    ->get();
+        } else if ($chart === 'route') {
+            $order = DB::table('weather')
+                    ->select(DB::raw("`weather_code` AS 'w_code', `weather_code`.`description` AS 'weather', `case`.`route_signing` AS 'r_code', `route_signing_code`.`description` AS 'route_signing', COUNT(*) AS 'count'"))
+                    ->leftJoin('weather_code', 'weather.weather_code', '=', 'weather_code.id')
+                    ->leftJoin('case', 'weather.casenum', '=', 'case.casenum')
+                    ->leftJoin('route_signing_code', 'case.route_signing', '=', 'route_signing_code.id')
+                    ->whereRaw('(`weather_code`.`id` > 0) AND (`case`.`route_signing` BETWEEN 0 AND 8) AND ' . $condition)
+                    ->groupby('w_code')
+                    ->groupby('case.route_signing')
+                    ->orderBy('w_code', 'asc')
+                    ->orderBy('case.route_signing', 'asc')
+                    ->get();
+        }
+        return $order;
     }
 
     private function drugAlcohol(Request $request) {
